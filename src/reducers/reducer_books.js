3995778c840945e187fa2d94b035954ebef5b5a7
Lou <lfranklin@deloitte.co.uk>
2016-06-23 09:47:06 +0100

    redux actions setup to display book details

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    { title: 'JS the good bits'},
-    { title: 'Harry Potter'},
-    { title: 'Dark Tower'},
-    { title: 'Red'}
+    { title: 'JS the good bits', pages: 100},
+    { title: 'Harry Potter', pages: 301},
+    { title: 'Dark Tower', pages: 46},
+    { title: 'Red', pages: 1982}
   ]
 }
